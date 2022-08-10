88512e214a95c2c5435740c2c463ae7255709277
Athea DeLing <almdeling@gmail.com>
2022-08-10 14:01:02 -0700

    add link on where and how to vote for over 18 and under 18
    Co-Author Samuel Majerus <ladolego@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 window.onload = function(){
   const form =document.querySelector("form");
   form.onsubmit = function(event) {
-    event.preventDefault();
+    //event.preventDefault();
 
-    let ballot = document.getElementById("vote")
-    ballot.setAttribute("class" ,"hidden"); 
+    const ageInput = parseInt(document.getElementById("age").value); 
+    let ballot = document.getElementById("vote"); 
+    ballot.setAttribute("class" ,"hidden");  
+    let tooYoung = document.getElementById("under-18"); 
 
-    
+    if(ageInput >= 18){ 
+      //Un-hide the ballot. 
+      ballot.removeAttribute("class"); 
+    }else{ 
+      //Keep ballot hidden, display 'under-18' msg & info-site links. 
+      tooYoung.removeAttribute("class"); 
+    } 
+
+    event.preventDefault(); 
   }
 }
